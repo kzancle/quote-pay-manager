d034fbcfe0ae054335aedcf329dd6869be6a73d5
kzancle <kevinsucle@gmail.com>
2026-06-22 19:04:56 -0300

    ADD python-docx to write the payment order with a temporary doc, just for tests

---
+++ poGenerator.py
@@ -0,0 +1,6 @@
+# Importa o python-docx
+from docx import Document
+
+# Pega o molde que será usado para fazer as ordens de pagamento (pegar o molde certo depois)
+documento = Document("molde_doc.docx")
+
