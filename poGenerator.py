fa743bf4dc7bfeebcc1091c601b24b2c5cb2607b
kzancle <kevinsucle@gmail.com>
2026-06-25 22:29:49 -0300

    ADD data format

--- poGenerator.py
+++ poGenerator.py
@@ -4,3 +4,26 @@ from docx import Document
 # Pega o molde que será usado para fazer as ordens de pagamento (pegar o molde certo depois)
 documento = Document("molde_doc.docx")
 
+# Dicionário para armazenar tudo que precisa ser inserido na OP
+
+itensDic = {
+    "solicitante": solicitante,
+    "dtSolicitacao": dtSolicitacao,
+    "CC1": cc1,
+    "CC2": cc2,
+    "fornecedor": fornecedor,
+    "tipoOp": tipoOp,
+    "descricao": descricao,
+    "dtPgtos": dataPgtos,
+    "valor": valor,
+    "formaPAgamento": formaPAgamento,
+    "pix": pix,
+    "titular": titular,
+    "banco": banco,
+    "agencia": agencia,
+    "contaCorrente": contaCorrente,
+    "cnpjCpf": cnpjCpf,
+    "status": status    
+}
+
+documento.save("docComInsercao.docx")
